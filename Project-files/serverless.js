c9279f12c2513cf35a1f2e62e9e665cb94e005c1
kerimdemirturk <kerimdmrturk@gmail.com>
2023-04-04 16:55:23 +0300

    added frontend files of project.

---
+++ Project-files/serverless.js
@@ -0,0 +1,45 @@
+var API_ENDPOINT = 'REPLACEME_API_GATEWAY_INVOKE_URL'; //paste your APIGATEWAY url and add /serverless app for url.
+var errorDiv = document.getElementById('error-message')
+var successDiv = document.getElementById('success-message')
+var resultsDiv = document.getElementById('results-message')
+
+// function output returns input button contents
+function waitSecondsValue() { return document.getElementById('waitSeconds').value }
+function messageValue() { return document.getElementById('message').value }
+function emailValue() { return document.getElementById('email').value }
+
+function clearNotifications() {
+    errorDiv.textContent = '';
+    resultsDiv.textContent = '';
+    successDiv.textContent = '';
+}
+
+// When buttons are clicked, this is run passing values to API Gateway call
+document.getElementById('emailButton').addEventListener('click', function(e) { sendData(e, 'email'); });
+
+function sendData (e, pref) {
+    e.preventDefault()
+    clearNotifications()
+    fetch(API_ENDPOINT, {
+        headers:{
+            "Content-type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            waitSeconds: waitSecondsValue(),
+            message: messageValue(),
+            email: emailValue()
+        }),
+        mode: 'cors'
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data)
+        successDiv.textContent = 'Submitted. But check the result below!';
+        resultsDiv.textContent = JSON.stringify(data);
+    })
+    .catch(function(err) {
+        errorDiv.textContent = 'Oops! Error Error:\n' + err.toString();
+        console.log(err)
+    });
+};
